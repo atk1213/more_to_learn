29d424967b0ad325c311fb5f89ae018baec9e7b8
Anthony Kim <anthonykim@ganthonykim.lan>
2020-03-23 23:03:30 -0700

    [prog] implemented React Context to utilize global state

--- intro_to_react_v5/src/Pet.js
+++ intro_to_react_v5/src/Pet.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from '@reach/router';
 // export default function Pet({ name, animal, breed }) {
 //   // return React.createElement("div", {}, [
 //   //   React.createElement("h1", {}, name),
@@ -19,14 +20,23 @@ export default function Pet({ name, animal, breed, media, location, id }) {
     hero = media[0].small;
   }
   return (
-    <a href={`/details/${id}`} className='pet'>
-      <div className={'image-container'}>
-        <img src={hero} alt={name}></img>
-      </div>
-      <div className='info'>
-        <h1>{name}</h1>
-        <h2>{`${animal} - ${breed} - ${location}`}</h2>
-      </div>
-    </a>
+    // <a href={`/details/${id}`} className='pet'>
+    //   <div className={'image-container'}>
+    //     <img src={hero} alt={name}></img>
+    //   </div>
+    //   <div className='info'>
+    //     <h1>{name}</h1>
+    //     <h2>{`${animal} - ${breed} - ${location}`}</h2>
+    //   </div>
+    // </a>
+        <Link to={`/details/${id}`} className='pet'>
+        <div className={'image-container'}>
+          <img src={hero} alt={name}></img>
+        </div>
+        <div className='info'>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${location}`}</h2>
+        </div>
+      </Link>
   )
 };
