8e9aeadf8de59a0200c53cb5385674c3f342283b
Anthony Kim <akim2214@gmail.com>
2020-04-04 23:30:58 -0700

    [prog] added query and mutation functionality to react code

--- GraphQL/client-graphql/client/src/pages/Pets.js
+++ GraphQL/client-graphql/client/src/pages/Pets.js
@@ -5,15 +5,54 @@ import PetsList from '../components/PetsList'
 import NewPetModal from '../components/NewPetModal'
 import Loader from '../components/Loader'
 
+const GET_PETS = gql`
+  query GetPets{
+    pets{
+      name
+      id
+      type
+      img
+    }
+  }
+`
+
+const ADD_PET = gql`
+  mutation CreateAPet ($newPet: NewPetInput!) {
+    addPet (input: $newPet){
+        name
+        id
+        type
+        img
+    }  
+  }
+`
 
 export default function Pets () {
   const [modal, setModal] = useState(false)
+  // useState will return an array
+    // 1st item in array is state we are keeping track of (modal)
+    // 2nd item in array is function used to update state (setModal)
+  const {data, loading, error} = useQuery(GET_PETS);
+  // returns an object with data, loading, and error
 
-
+  // const [addPet, {data, loading, error}] = useMutation(ADD_PET)
+  const [addPet, newPet] = useMutation(ADD_PET)
+  // 1st item is the query function
+  // second item is an object with data, loading, and error
+  
   const onSubmit = input => {
     setModal(false)
+    // input = type and name
+    addPet({
+      variables: {
+        newPet: input
+      }
+    })
   }
   
+  if (loading || newPet.loading) {return <Loader />}
+  if (error || newPet.error) {console.error(error)}
+
   if (modal) {
     return <NewPetModal onSubmit={onSubmit} onCancel={() => setModal(false)} />
   }
@@ -32,7 +71,7 @@ export default function Pets () {
         </div>
       </section>
       <section>
-        <PetsList />
+        <PetsList pets={data.pets} />
       </section>
     </div>
   )
